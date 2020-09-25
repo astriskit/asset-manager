193232e8ec9a71fce6c9b53e9a943561875d4c22
astriskit <harry2office@gmail.com>
2020-09-25 16:26:20 +0530

    release v2.0

---
+++ docs/precache-manifest.63e05e645887e49379d5524c9e09b31c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a11f9a226216b8e203f9652efabc5df",
+    "url": "/asset-manager/index.html"
+  },
+  {
+    "revision": "55a276ef93760f4d15e5",
+    "url": "/asset-manager/static/css/2.cd846464.chunk.css"
+  },
+  {
+    "revision": "444d2cbdf5d069a8503a",
+    "url": "/asset-manager/static/css/main.1841c45a.chunk.css"
+  },
+  {
+    "revision": "55a276ef93760f4d15e5",
+    "url": "/asset-manager/static/js/2.bdd84fe5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/asset-manager/static/js/2.bdd84fe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "444d2cbdf5d069a8503a",
+    "url": "/asset-manager/static/js/main.9ff31833.chunk.js"
+  },
+  {
+    "revision": "e81b8cbe4b970bcfdbd4",
+    "url": "/asset-manager/static/js/runtime-main.89462d6c.js"
+  }
+]);
